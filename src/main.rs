f9f74f922e1549fc699f6e5a3633cd0e3ab50458
lhh <776334655@qq.com>
2019-09-30 10:34:53 +0800

    match 表达式

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 fn main() {
-    let number = 3;
-    match number {
-        0 => println!("Origin"),
-        1..=3 => println!("All"),
-        n @ 3 | n @ 4 | n @ 13 => println!("Bad Luck {}", n),
-        n @ 42 => println!("answer is {}", n),
-        _ => println!("common"),
+    let boolean = true;
+    let mut binary = 0;
+    if let true = boolean {
+        binary = 1;
     }
+    println!("{}",binary)
 }
