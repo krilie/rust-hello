3f2d59d97c51a550d6094e328c65d4b6eaa5cb26
lhh <776334655@qq.com>
2019-09-30 10:12:39 +0800

    for

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let n = 13f32;
-    let big_n = if n < 10f32 && n > -10f32 { 10f32 * n } else { n / 2f32 };
-    println!("{}", big_n)
+    for n in 1..10000000 {
+        println!("{}",n);
+    }
 }
