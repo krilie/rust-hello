e6e2e28bae63a6b243b0f8994862dcbc6e26a503
lhh <776334655@qq.com>
2019-09-30 10:52:18 +0800

    范围类型

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
 fn main() {
-    let x = true;
-    let y: bool = false;
-    let x = 5;
-    if x > 1 {
-        println!("x val {}", x)
-    };
-    println!("{}", x as i32);
-    println!("{}", y as i32);
+    let val = (1..5);
+    println!("{:?}",val);
 }
