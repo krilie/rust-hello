6a17498ada5869f317ca29c64f37b26d02bd5e38
lhh <776334655@qq.com>
2019-09-30 10:29:23 +0800

    match 表达式

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let number = 42;
     match number {
         0 => println!("Origin"),
-        1...3 => println!("All"),
+        1..=3 => println!("All"),
         3 | 4 | 13 => println!("Bad Luck"),
         n @ 42 => println!("answer is {}", n),
         _ => println!("common"),
