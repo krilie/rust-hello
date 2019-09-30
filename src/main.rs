d441ca7b8015dcb5db2a842363972b597d3b0234
lhh <776334655@qq.com>
2019-09-30 09:55:15 +0800

    函数做为参数

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
+fn closure_math<F: Fn() -> i32>(op: F) -> i32 {
+    op()
+}
+
 fn main() {
-    let out = 42;
-//    fn add(i: i32, j: i32) -> i32 {
-//        i + j + out
-//    };
-    let closure_annotated = |i: i32, j: i32| -> i32 { i + j + out };
-    let closure_inferred = |i, j| i + j + out;
-    let i = 1;
-    let j = 2;
-    print!("{}\n", closure_annotated(i, j));
-    print!("{}\n", closure_inferred(i, j))
+    let a = 2;
+    let b = 3;
+    println!("{}", closure_math(|| a + b));
+    println!("{}", closure_math(|| a * b));
 }
