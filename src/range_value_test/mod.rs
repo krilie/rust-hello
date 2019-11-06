8d499150f512a154ba5fb1977d5ea495d7bf351e
lhh <776334655@qq.com>
2019-11-06 11:43:08 +0800

    左值右值

--- src/range_value_test/mod.rs
+++ src/range_value_test/mod.rs
@@ -1,7 +1,9 @@
 mod range_test;
 mod slice_type;
+mod str_type;
 
 pub fn test() {
     range_test::test();
     slice_type::test();
+    str_type::test();
 }
---
+++ src/range_value_test/str_type.rs
@@ -0,0 +1,11 @@
+pub fn test(){
+    let truth:&'static str = "一个苹果。。aa";
+    let ptr = truth.as_ptr();
+    let len = truth.len();
+    println!("{:?} {:?}",ptr,len);
+    let s = unsafe {
+        let slice = std::slice::from_raw_parts(ptr,len);
+        std::str::from_utf8(slice)
+    };
+    println!("{:?}",s)
+}
