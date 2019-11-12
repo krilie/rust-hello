386fcc5d95efdeec4c216247a82ad3cf72d45d57
lhh <776334655@qq.com>
2019-11-12 11:36:38 +0800

    类型 string

--- src/type_sys/mod.rs
+++ src/type_sys/mod.rs
@@ -2,4 +2,5 @@ mod string_test;
 
 pub fn test(){
     string_test::test();
+    string_test::test2();
 }
--- src/type_sys/string_test.rs
+++ src/type_sys/string_test.rs
@@ -4,3 +4,8 @@ pub fn test(){
     let len = str.len();
     println!("{:p} len:{:?} str:{:?}",ptr,len,str);
 }
+
+pub fn test2(){
+    println!("{:?}", std::mem::size_of::<&[u32;5]>());
+    println!("{:?}", std::mem::size_of::<&mut [u32]>());
+}
