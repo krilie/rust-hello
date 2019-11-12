06ceea9bee0a4ed8525b85405fd13a95ad26bd19
lhh <776334655@qq.com>
2019-11-12 09:45:13 +0800

    泛型

--- src/generics_type/generics_1.rs
+++ src/generics_type/generics_1.rs
@@ -1,13 +1,13 @@
 use std::fmt::Debug;
 
-fn match_option<T:Debug>(o:Option<T>){
+fn match_option<T: Debug>(o: Option<T>) {
     match o {
-        Some(i) => println!("{:?}",i),
+        Some(i) => println!("{:?}", i),
         None => println!("nothing"),
     }
 }
 
-pub fn test(){
+pub fn test() {
     let a = Some(3);
     match_option(a);
 }
