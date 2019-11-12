fb995ddc5d1131e8cf08c26446b0e275e88f5068
lhh <776334655@qq.com>
2019-11-12 10:43:23 +0800

    泛型

--- src/generics_type/mod.rs
+++ src/generics_type/mod.rs
@@ -1,5 +1,8 @@
 pub mod generics_1;
+mod trait_test;
 
 pub fn test(){
     generics_1::test();
+    trait_test::test();
+    trait_test::debug_trait::test();
 }
---
+++ src/generics_type/trait_test.rs
@@ -0,0 +1,46 @@
+struct Duck;
+struct Pig;
+trait Fly {
+    fn fly(&self) -> bool;
+}
+impl Fly for Duck {
+    fn fly(&self) -> bool {
+        return true;
+    }
+}
+impl Fly for Pig {
+    fn fly(&self) -> bool {
+        false
+    }
+}
+
+fn fly_static<T: Fly>(s: T) -> bool {
+    s.fly()
+}
+fn fly_dyn(s: &impl Fly) -> bool {
+    s.fly()
+}
+
+pub fn test() {
+    let pig = Pig;
+    println!("{:?}", fly_static(pig));
+    let duck = Duck;
+    println!("{:?}", fly_dyn(&duck));
+}
+
+pub mod debug_trait {
+    use std::fmt::*;
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+    impl Debug for Point {
+        fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+            write!(f, "Point ---- == == x:{} y:{}", self.x, self.y)
+        }
+    }
+    pub fn test() {
+        let origin = Point { x: 0, y: 0 };
+        println!("{:?}", origin);
+    }
+}
