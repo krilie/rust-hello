7895d1ef2a85ff5b1bdf58c2fe81816a7251ec49
lhh <776334655@qq.com>
2019-11-14 16:43:48 +0800

    借用规则

--- src/fun_closure_iter/mod.rs
+++ src/fun_closure_iter/mod.rs
@@ -1,6 +1,7 @@
 mod func_define;
 mod func_mut;
 mod func_for_struct;
+mod func_ptr_2;
 
 pub fn test(){
     func_define::test();
@@ -10,4 +11,5 @@ pub fn test(){
     func_mut::test4();
     func_mut::test5();
     func_for_struct::test();
+    func_ptr_2::test();
 }
