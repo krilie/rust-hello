a66c825f1c1a382a54fb1a95927e28494409492c
lhh <776334655@qq.com>
2019-11-15 14:10:34 +0800

    collect iter

--- src/arch_test/collection_test.rs
+++ src/arch_test/collection_test.rs
@@ -3,4 +3,9 @@ pub fn test() {
     let tao = str::from_utf8(&[0xe9u8, 0x81u8, 0x93u8]).unwrap();
     println!("{}", tao);
     println!("{:?}", tao.chars());
+    let tao = '藏';
+    println!("{:?}", tao);
+    println!("{:?}", tao.is_uppercase());
+    println!("{:?}", tao.is_lowercase());
+    println!("{:?}", tao.is_alphabetic());
 }
