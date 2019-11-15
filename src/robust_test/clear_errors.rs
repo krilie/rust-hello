056133c24ca1972d2ed5f33f7c9501a47b42b4e8
lhh <776334655@qq.com>
2019-11-15 15:26:48 +0800

    collect iter

--- src/robust_test/clear_errors.rs
+++ src/robust_test/clear_errors.rs
@@ -1,7 +1,7 @@
-fn sum(a:i32,b:i32)->i32{
-    a+b
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
 }
-pub fn test(){
-//    sum(1u32,2u32);
+pub fn test() {
+    //    sum(1u32,2u32);
     println!("{:?}", sum(23, 23));
 }
