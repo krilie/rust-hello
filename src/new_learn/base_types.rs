292e040af322f57e0ddee1f61b335646d3f2e35e
acorn <1197829331@qq.com>
2019-12-08 21:54:31 +0800

    字符串 与 指针

--- src/new_learn/base_types.rs
+++ src/new_learn/base_types.rs
@@ -13,3 +13,16 @@ fn test_slice() {
     println!("{:?}", vec);
     println!("{:?}", &vec[1..]);
 }
+
+#[test]
+fn test_str() {
+    let truth: &'static str = "Rust 是一门优雅的语言";
+    let ptr = truth.as_ptr();
+    let len = truth.len();
+    println!("len {}", len);
+    let s = unsafe {
+        let slice = std::slice::from_raw_parts(ptr, len);
+        std::str::from_utf8(slice)
+    };
+    println!("s- {:?}\ntruth- {:?}", s, truth);
+}
