5118b73f81c2ddecd1a5ad14204f94bb66823bce
acorn <1197829331@qq.com>
2019-12-08 21:49:46 +0800

    vec 切片

--- src/new_learn/base_types.rs
+++ src/new_learn/base_types.rs
@@ -1,9 +1,15 @@
+#[test]
+fn number_test() {
+    println!("{:?}", std::f32::INFINITY);
+    println!("{:?}", std::f32::NEG_INFINITY);
+    println!("{:?}", std::f32::NAN);
+    println!("{:?}", std::f32::MIN);
+    println!("{:?}", std::f32::MAX);
+}
 
 #[test]
-fn number_test(){
-    println!("{:?}",std::f32::INFINITY);
-    println!("{:?}",std::f32::NEG_INFINITY);
-    println!("{:?}",std::f32::NAN);
-    println!("{:?}",std::f32::MIN);
-    println!("{:?}",std::f32::MAX);
+fn test_slice() {
+    let vec = vec![1, 2, 3, 4, 5];
+    println!("{:?}", vec);
+    println!("{:?}", &vec[1..]);
 }
