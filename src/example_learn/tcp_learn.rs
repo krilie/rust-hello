867010085bc764b5c1c05261b24492166caea858
lhh <776334655@qq.com>
2020-01-04 09:15:21 +0800

    tcp echo 服务

--- src/example_learn/tcp_learn.rs
+++ src/example_learn/tcp_learn.rs
@@ -1,6 +1,40 @@
-use std::net::TcpStream;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::io::Read;
+use std::io::Write;
+
+fn handle_client(mut stream: TcpStream) {
+    // read 20 bytes at a time from stream echoing back to stream
+    loop {
+        let mut read = [0; 1028];
+        match stream.read(&mut read) {
+            Ok(n) => {
+                if n == 0 {
+                    // connection was closed
+                    break;
+                }
+                stream.write(&read[0..n]).unwrap();
+            }
+            Err(err) => {
+                panic!(err);
+            }
+        }
+    }
+}
 
 #[test]
-fn tcp_get() {
-    println!("hello world");
-}
+fn test_tcp() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
+            }
+            Err(_) => {
+                println!("Error");
+            }
+        }
+    }
