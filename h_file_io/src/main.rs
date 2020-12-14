18d350c42402db6e80b970a32518172153518c96
Administrator <1197829331@qq.com>
2020-12-14 15:36:50 +0800

    读写文件

---
+++ h_file_io/src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+// create file and write something
+fn create_file(filename: &str, buf: &[u8]) -> io::Result<()> {
+    let mut f = File::create(filename).unwrap();
+    f.write(&buf).unwrap();
+    Ok(())
+}
+
+// read from file to String
+fn read_file(filename: &str, buf: &mut String) -> io::Result<()> {
+    let mut f = File::open(filename).unwrap();
+    f.read_to_string(buf).unwrap();
+    Ok(())
+}
+
+fn main() {
+    let f = "foo.txt";
+    let mut buf = String::new();
+    match create_file(f, b"Hello, World!") {
+        Ok(()) => {
+            match read_file(f, &mut buf) {
+                Ok(()) => { println!("{}", buf); }
+                Err(e) => { println!("{}", e); }
+            };
+        }
+        Err(e) => { println!("{}", e); }
+    }
+}
