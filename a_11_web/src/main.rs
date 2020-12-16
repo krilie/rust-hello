f3101d7c4681c122dfaa23db7a3cee7c26685431
Administrator <1197829331@qq.com>
2020-12-16 14:31:30 +0800

    web

--- a_11_web/src/main.rs
+++ a_11_web/src/main.rs
@@ -35,3 +35,42 @@ mod one {
         println!("On 3000");
     }
 }
+
+mod two{
+    extern crate iron;
+    extern crate router;
+    extern crate rustc_serialize;
+
+    use iron::prelude::*;
+    use iron::status;
+    use router::Router;
+    use rustc_serialize::json;
+    use std::io::Read;
+
+    #[derive(RustcEncodable, RustcDecodable)]
+    struct Greeting {
+        msg: String
+    }
+    #[test]
+    fn main() {
+        let mut routers = Router::new();
+        routers.get("/", hello_world,"1");
+        routers.get("/set", set_greeting,"2");
+
+        fn hello_world(_: &mut Request) -> IronResult<Response> {
+            let greeting = Greeting { msg: "Hello, World".to_string() };
+            let payload = json::encode(&greeting).unwrap();
+            Ok(Response::with((status::Ok, payload)))
+        }
+
+        fn set_greeting(request: &mut Request) -> IronResult<Response> {
+            let mut payload = String::new();
+            request.body.read_to_string(&mut payload);
+            let request: Greeting = json::decode(&payload).unwrap();
+            let greeting = Greeting { msg: request.msg };
+            let payload = json::encode(&greeting).unwrap();
+            Ok(Response::with((status::Ok, payload)))
+        }
+        Iron::new(routers).http("localhost:3000").unwrap();
+    }
+}
